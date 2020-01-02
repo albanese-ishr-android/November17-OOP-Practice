d3be94e6e79dc4b2c4c80a078c7a5383a3383cf8
tony-albanese <tony.albanese2000@gmail.com>
2020-01-02 14:45:22 +0100

    Fixed some errors.

--- src/Payment.java
+++ src/Payment.java
@@ -3,6 +3,7 @@ public class Payment {
     private static double foodTax = 0.2; // 20 % sales tax added to
     private static double drinkTax = 0.1; // 10 % sales tax added to
     private int fiCount;
+
     // all food prices
     private DrinkItem[] di = new DrinkItem[100];
     private int diCount;
@@ -14,13 +15,23 @@ public class Payment {
     }
 
     // all other accessor and mutator methods are included
-// addFoodItem() – this method adds a new FoodItem object
-// addDrinkItem() – this method adds a new DrinkItem object
-    public static double findPrice(Item[] pl, String c) { //code missing }
-// calculateBill() – This method returns the bill (the total value of
-// the items consumed for a particular table)
+    // addFoodItem() – this method adds a new FoodItem object
+    // addDrinkItem() – this method adds a new DrinkItem object
+
+    public static double findPrice(Item[] pl, String c) {
+        //code missing
+
+        return 0; //This is not the right return value.
     }
+
+/*
+calculateBill() {
+// This method returns the bill (the total value of the items consumed for a particular table)
+    }
+ */
 
     public DrinkItem getDi(int x) {
         return di[x];
     }
+
+}
