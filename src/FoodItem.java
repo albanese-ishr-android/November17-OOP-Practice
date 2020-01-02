f8c7bf1ccff2ac520a606792a18e2f0cefd33b86
tony-albanese <tony.albanese2000@gmail.com>
2020-01-02 15:26:02 +0100

    Completed TODOs 1-4

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,6 +1,23 @@
 public class FoodItem {
 
     //TODO: 3 Using two examples from this class (or any other one) explain the benefits of using different data types.
+
+    /*
+    Having different data types allow different operations to be carried out depending
+upon the type;
+eg
+Calculations in the integer “quantity” variable;
+which are not possible for a String;
+Double instead of integer allows the use of decimals;
+which mirrors real-life scenarios/allows more precise calculations;
+Using an array (of objects);
+Allows individual items to be accessed/processed more easily;
+Memory usage can be reduced;
+eg “fiCount” takes up less space as an integer than it would as a double
+(assuming it’s just a number);
+     */
+
+
     private String itemCode;
     private int quantity;
 
--- src/Payment.java
+++ src/Payment.java
@@ -1,27 +1,39 @@
 public class Payment {
 
     //TODO: 2 - State the implication of the modifier "static" in this class.
+    /*
+    EITHER
+    The values of these (static) variables;
+    Are the same for all objects;
+    They belong to the class (not to the objects);
+    And are only created/declared once;
+    Note: Do not accept “cannot be changed”.
+    OR
+    The static methods;
+    Are class methods (not object methods);
+    Are independent of the objects in the class;
+     */
 
     private static double foodTax = 0.2; // 20 % sales tax added to
     private static double drinkTax = 0.1; // 10 % sales tax added to
-    private int fiCount;
+
+    private int fiCount = 0;
 
     // all food prices
     private DrinkItem[] di = new DrinkItem[100];
-    private int diCount;
+    private int diCount = 0;
 
     // all drink prices
-    //TODO: 1 How could we initialize these variables without using the constructor?
+    //TODO: 1 How could we initialize these variables without using the constructor? Initialize in the declation.
     public Payment() {
-        fiCount = 0;
-        diCount = 0;
+
     }
 
     // all other accessor and mutator methods are included
 
     //TODO: 5 The Payment class method addFoodItem() is passed a FoodItem object as a parameter. Complete this method.
 
-    // addFoodItem() – this method adds a new FoodItem object
+    addFoodItem()
 
 
     // addDrinkItem() – this method adds a new DrinkItem object
