632732841ee0850a139a88ba0b1161526a11e798
tony-albanese <tony.albanese2000@gmail.com>
2020-01-02 14:46:39 +0100

    Added getters and setters.

--- src/Item.java
+++ src/Item.java
@@ -2,5 +2,33 @@ public class Item {
     private String code; // item code
     private String name; // item name
     private double price; // unit price before tax
-// all accessor, mutator and constructor methods are included
+
+
+    /*
+    These getters and setters are not in the exam paper. They are there though. They are not printed just to save
+    paper.
+     */
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
