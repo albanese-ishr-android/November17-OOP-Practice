78fe176200dec43e2faa27033332da60663bc708
tony-albanese <tony.albanese2000@gmail.com>
2020-01-20 20:20:28 +0100

    Added member variables and getters/setters

--- src/DrinkItem.java
+++ src/DrinkItem.java
@@ -1,2 +1,30 @@
 public class DrinkItem {
+
+    private String itemCode;
+    private int quantity;
+
+    public DrinkItem(String x, int y) {
+        itemCode = x;
+        quantity = y;
+    }
+
+
+    /*
+    Getters and setters are not in the exam paper but they are there.
+     */
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
