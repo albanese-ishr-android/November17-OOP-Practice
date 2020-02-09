bd35629bc00ff88df3a136b5658278b2da03f96a
tony-albanese <tony.albanese2000@gmail.com>
2020-02-09 14:56:20 +0100

    Commented out code.

--- src/RestaurantDriver.java
+++ src/RestaurantDriver.java
@@ -24,14 +24,14 @@ public class RestaurantDriver {
         tables[2] = new Payment();
         FoodItem a = new FoodItem("f102", 2);
         FoodItem b = new FoodItem("f100", 1);
-        DrinkItem c = new DrinkItem("d102", 3);
-        tables[1].addFoodItem(a);
-        tables[1].addFoodItem(b);
-        tables[2].addDrinkItem(c);
-        tables[2].addDrinkItem(new DrinkItem("d103", 1));
-        System.out.println(tables[1].getFiCount());
-        System.out.println(Payment.getFoodTax());
-        System.out.println(tables[2].getDi(1).getItemCode());
+        //DrinkItem c = new DrinkItem("d102", 3);
+        //tables[1].addFoodItem(a);
+        //tables[1].addFoodItem(b);
+        //tables[2].addDrinkItem(c);
+        //tables[2].addDrinkItem(new DrinkItem("d103", 1));
+        //System.out.println(tables[1].getFiCount());
+        //System.out.println(Payment.getFoodTax());
+        //System.out.println(tables[2].getDi(1).getItemCode());
 
 
         /*
